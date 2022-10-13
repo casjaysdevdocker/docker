28b2f9062a6687e30ecb9b116190402089dcd18a
casjay <git-admin@casjaysdev.com>
2022-10-13 19:18:47 -0400

    🗃️ Committing everything that changed 🗃️

--- bin/entrypoint-docker.sh
+++ bin/entrypoint-docker.sh
@@ -186,7 +186,7 @@ docker)
       fi
       if [ ! -f "/run/dockerd.pid" ]; then
         echo "Starting dockerd on port 2375 and /var/run/docker.sock"
-        dockerd --config-file /config/docker/daemon.json
+        dockerd -H tcp://127.0.0.1:2375 -H unix://var/run/docker.sock --config-file /config/docker/daemon.json
       fi
     fi
   else
